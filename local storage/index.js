a81e83e1ada42590b53501652c83f66d8e9cd929
Aayush Basnet <aayushbasnet07@gmail.com>
2022-01-10 23:29:15 +0545

    play btn `

---
+++ local storage/index.js
@@ -0,0 +1,10 @@
+const rgbColor = document.querySelector('#rgbCode');
+const easyBtn = document.querySelector('#easyBtn');
+const hardBtn = document.querySelector('#hardBtn');
+const colorBox = document.querySelector('.color-box');
+const box = document.querySelectorAll('.box');
+const playBtn = document.querySelector('playAgainBtn');
+easyBtn.addEventListener('click', e => {
+    const boxValue = 3;
+    
+});
